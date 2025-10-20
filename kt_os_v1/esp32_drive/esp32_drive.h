0dd91375d735ef609a318cd75ab66b9739178290
kteer1 <1194202735@qq.com>
2025-10-20 12:24:42 +0800

    删除kt_os_v1

--- kt_os_v1/esp32_drive/esp32_drive.h
+++
@@ -1,9 +0,0 @@
-#ifndef ___kESP32_DRIVE_H__
-#define ___kESP32_DRIVE_H__
-
-
-
-
-#endif // ESP32_DRIVE_H
-
-
--- kt_os_v1/kt_os/kt_os.h
+++
@@ -1,8 +0,0 @@
-#ifndef ___kKT_OS_H__
-#define ___kKT_OS_H__
-
-void KtOs_Init(void* param);
-
-#endif // KT_OS_H
-
-
